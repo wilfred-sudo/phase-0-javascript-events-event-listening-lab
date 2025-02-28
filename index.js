c6da0ab8794feb9feb884b58c5d41760e353c825
wilfredkiprop <kipropwilfred90@gmail.com>
2025-02-28 17:38:59 +0300

    event lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    const button = document.getElementById("button");
+    if (button) { 
+        button.addEventListener("click", function() {
+            alert("button clicked!");
+        });
+    } else {
+        console.error("Element with ID 'button' not found!");
+    }
 }
